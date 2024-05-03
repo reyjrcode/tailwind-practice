2a6afc49ee9daffdf547e2830952b2dcf7c87302
reyjrcode <rey.alcalajr.code@gmail.com>
2024-05-03 10:58:03 +0800

    shoe detail layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 
 import './App.css'
 import { Nav } from '../src/components/Nav.jsx'
+import {ShoeDetail} from '../src/components/ShoeDetail.jsx'
 
 function App() {
-  return <div className='"p-10 xl:px-24'>
+  return <div className='p-10 xl:px-24'>
     <Nav/>
+    <ShoeDetail/>
   </div>;
 }
 
